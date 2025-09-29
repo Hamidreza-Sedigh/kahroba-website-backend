d6fd90932752d3eddb0ecd297676702d40462a0c
Hamid Reza Sedigh <hamidr.sedigh@gmail.com>
2025-09-29 22:42:13 +0330

    'dev'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,6 @@ module.exports = {
       {
         name: "website-backend",
         script: "src/server.js",
-        instances: 2,                         // تعداد پردازش‌ها (به اندازه CPU 2 core)
-        exec_mode: "cluster",                 // حالت کلاستر برای استفاده از چند هسته
-        max_memory_restart: "2G",             // اگر بیشتر از 1 گیگ رم مصرف کرد ری‌استارت بشه
-        watch: false,                         // تغییر فایل‌ها ری‌استارت نکنه (مناسب پروداکشن)
         env: {
           NODE_ENV: "development"
         },
